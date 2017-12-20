aeccee820235460f43fa35fff34ca16da020641e
kimrharper <kimrharper@gmail.com>
2017-12-20 09:52:55 -0800

    added two rooms

---
+++ noir_items.py
@@ -0,0 +1,9 @@
+items = ["gerbil", "key", "candybars", ]
+
+class item:
+    self.description = description      ##string
+    self.visible = visible              ##boolean
+    self.clue_ranking = clue_ranking    ##0 or 1
+    self.mutable = mutable              ##boolean
+    self.notebook = notebook            ##string
+    self.space_amount = space_amount    ##integer
---
+++ noir_rooms.py
@@ -0,0 +1,29 @@
+big_rooms_dict = [classroom, teacherdesk,student1desk,chalkboard,trashcan]
+                  [gym,bleachers,fountain,room3,room4]
+                  [lockerroom,locker1,locker2,trashcan,bench]
+                  [cafeteria, lunchline,lunchtable,doorway,kitchen]
+                  [gym,swings,monkeybars,bballcourt]
+                  [principal office, princdesk,shelf1,shelf2]
+
+move_count = 0
+start_point = "classroom"
+
+classroom = room(classroom,"There is an empty classroom with a chalkboard and desks.", None)
+teacherdesk = room(teacherdesk,"There is a teacher's desk by the chalkboard", classroom)
+
+
+class room(self,name,description,big_room):
+    self.name = name
+    self.description = description      ##string
+    self.inventory = inventory          ##list[item (or) character]
+    self.connects_to = big_room         ##list[rooms]
+    self.open_req = []
+
+          ##dict{item: text, item:text}
+
+    # self.move_distance = move_distance  ##integer
+    # self.trigger = trigger              ##string
+
+
+    def move_to(self):
+        self.move_count = self.move_distance + 1
